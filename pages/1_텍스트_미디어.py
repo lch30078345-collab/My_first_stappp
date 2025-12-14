6712e691df484ca6b13393ac0ca37241e0bdaf1d
lch30078345-collab <lch30078345@gmail.com>
2025-12-14 16:19:28 +0900

    Add files via upload

---
+++ pages/1_텍스트_미디어.py
@@ -0,0 +1,68 @@
+'## 🆎 : 일반 텍스트'
+
+import streamlit as st
+
+st.title('제목 : st.title()')
+st.header('헤더 : st.header()')
+st.subheader('서브헤더 : st.subheader()')
+st.text('본문 텍스트 : st.text()')
+st.markdown('## 마크다운 : st.markdown()')
+st.caption('캡션(작고 흐린 글씨로 표현됨) : st.caption()')
+
+'# 🆎 : st.write()'
+st.write('# 마크다운 H1 : st.write()')
+st.write('### 마크다운 H3 : st.write()')
+st.write('')  # 빈 줄 추가
+
+
+'# 🆎 : 색상이 있는 텍스트'
+st.write(':red[빨간색 텍스트]')
+st.write(':blue[파란색 텍스트]')
+
+
+'### 코드 블록: st.code()'
+st.code('print("Hello, World!")', language='python', line_numbers=True)
+
+'### 코드+결과: st.echo()'
+with st.echo():
+    # 이 블록의 코드와 결과를 출력
+    name = 'Chunghun Ha'
+    st.write("Hello, Streamlit!", name)
+
+'### Latex 수식 작성: st.latex()'
+st.latex(r'\int_a^b f(x)dx')
+
+st.divider()  # 👉 구분선
+
+
+---
+
+## 📌 텍스트 · 미디어 (`텍스트_미디어.py`)
+
+
+'# 🎥 : 미디어 삽입'
+
+'### :orange[이미지: st.image()]'
+st.image("./data/python.png", caption="파이썬 로고", use_container_width=True)
+
+'### :orange[오디오: st.audio()]'
+st.audio("./data/After_You.mp3", format="audio/mpeg", loop=True)
+
+'### :orange[동영상: st.video()]'
+st.video("./data/stars.mp4", format="video/mp4", loop=True)
+st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")  # YouTube 링크
+
+
+'# 📊 : 콜아웃'
+
+'### :orange[정보: st.info()]'
+st.info('This is a purely informational message', icon="ℹ️")
+
+'### :orange[경고: st.warning()]'
+st.warning('This is a warning message', icon="⚠️")
+
+'### :orange[에러: st.error()]'
+st.error('This is an error message', icon="⛔")
+
+'### :orange[성공: st.success()]'
+st.success('This is a success message', icon="✅")
---
+++ pages/2_데이터_그래프.py
@@ -0,0 +1,93 @@
+import streamlit as st
+
+'### :orange[Pandas 데이터프레임]'
+import pandas as pd
+
+df = pd.DataFrame(
+    {'id': [1, 2, 3],
+        'name': ['Alice', 'Bob', 'Charlie'],
+        'age': [24, 34, 45]
+    }
+)
+
+df  # 👉 데이터프레임 출력
+
+'### :orange[지표(Metric)]'
+col1, col2, col3 = st.columns(3)  # 3개의 컬럼 생성
+
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
+
+
+'# :blue[Streamlit 그래프]'
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"]
+)
+
+'#### :orange[st.area_chart()]'
+st.area_chart(chart_data)
+
+'#### :orange[st.line_chart()]'
+st.line_chart(chart_data)
+
+'#### :orange[st.bar_chart()]'
+st.bar_chart(chart_data)
+
+'#### :orange[st.scatter_chart()]'
+st.scatter_chart(chart_data)
+
+'#### :orange[st.map()]'
+df = pd.DataFrame(
+    np.random.randn(100, 2) / [100, 100] + [37.55, 126.92],
+    columns=["lat", "lon"],
+)
+st.map(df)
+
+
+'### :orange[Matplotlib: st.pyplot()]'
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+st.pyplot(fig)  # 👉 차트 출력
+
+st.divider()  # 👉 구분선
+
+'### :orange[Altair: st.altair_chart()]'
+import altair as alt
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"]
+)
+
+c = (
+    alt.Chart(chart_data)
+    .mark_circle()
+    .encode(
+        x="a", y="b",
+        size="c",
+        color="c",
+        tooltip=["a", "b", "c"]
+    )
+)
+
+st.altair_chart(c, use_container_width=True)
+
+'### :orange[Plotly: st.plotly_chart()]'
+import plotly.express as px
+
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length")
+
+st.plotly_chart(fig, key = 'iris', on_select = 'return')
